37b697110186d11c7659489aad9fa4f472fee254
valeriy.rumak <valeriy.rumak@youshido.com>
2020-10-26 04:11:57 +0200

    husky test

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { theme } from '@Definitions/Styled';
 import Home from '@Scenes/Home';
 
 const Stack = createStackNavigator();
